d62b70658ac4c7d9efcb249952dc778c6939aa55
Polok <polok.hasibul@gmail.com>
2019-12-13 21:47:46 +0600

    Added Files

---
+++ GPS/views.py
@@ -0,0 +1,36 @@
+from django.http import *
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
+from Location.forms import *
+#from Employee.forms import *
+
+
+
+def gps(request):
+    data = dict()
+    if request.method == "POST":
+        CarID = request.POST.get("CarID")
+        Latitude = request.POST.get("Latitude")
+        Longitude = request.POST.get("Longitude")
+        GPS.object.create(
+            Longitude = Longitude,
+            Latitude = Latitude,
+            CarID = CarID,
+            )
+
+    return JsonResponse(data)
+def gpspost(request):
+	if request.method == 'POST':
+		form = gps_form(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/showgps/')
+	else:
+		form = gps_form()
+	return render(request,'postgps.html',{'form':form})
+
+def showgps(request):
+	context=Gps.objects.all()
+
+	return render(request,'showgps.html',{"context":context})
---
+++ Location/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Gps(models.Model):
+	CarID = models.CharField(max_length=30)
+	Latitude = models.CharField(max_length=30)
+	Longitude = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.CarID
+# Create your models here.
